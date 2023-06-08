447908e395a1f8e43e174ad7da490750e2a34087
0ignacio <ignaciogonzaley@gmail.com>
2023-06-08 19:32:05 -0300

    punto2 2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.IO;
 
+List<string> archivos = Directory.GetDirectories("C:\tp8\tl1_tp8_2023-0ignacio").ToList();
+
+foreach (string archivo in archivos)
+{
+    string[] partes = archivo.Split('.');
+}
